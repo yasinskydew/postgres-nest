9d073e34a9256b0718b226cae9c66a526bef9a30
vladimir Yasinsky <uladzimir.yasinski@webfriends.by>
2020-10-20 18:13:19 +0300

    add nest logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   await app.listen(3000, () => {
     console.log('postgres app listen to port 3000');
   });
